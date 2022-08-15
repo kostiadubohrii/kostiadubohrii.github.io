9844a24e2a78fa6eee38d1b525d9682c1077eaf8
= <kostadubogriy@gmail.com>
2022-08-15 22:53:25 +0100

    add js

---
+++ web-sites/pulse/src/js/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/right.png"></button>',
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                dots: true,
+                arrows: false
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    });
+});
