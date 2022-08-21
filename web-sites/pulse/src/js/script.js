754d4b98845c8d893a59873a79345f547c0c5902
= <kostadubogriy@gmail.com>
2022-08-21 12:03:57 +0100

    tabs + forms added

--- web-sites/pulse/src/js/script.js
+++ web-sites/pulse/src/js/script.js
@@ -39,5 +39,52 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
+    // Modal
 
+    $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('fast');
+    });
+
+    $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #order, #thanks').fadeOut('fast');
+    });
+
+    $('.button_mini').each(function (i) {
+      $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('fast');
+      });
+    });
+
+
+
+    function validateForm(form) {
+      $(form).validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2
+          },
+          phone: "required",
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          name: {
+            required: true,
+            minlength: jQuery.validator.format("Введите {0} символов!")
+          },
+          phone: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Подалуйста, введите свою почту",
+            email: "Неправильно введен адрес почты"
+          }
+        }
+    });
+  }
+    validateForm('#order form');
+    validateForm('#consultation-form');
+    validateForm('#consultation form');
 });
